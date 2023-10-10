24cd0a023d177c42cdc3cdc588c47e8650e7d71a
WILLIAM ELLIS <will@wearestudio77.com>
2023-10-10 15:20:05 +0100

    added css and bits

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect} from 'react'
+
 
 function GameList() {
     const urlTime = new Date()
@@ -13,16 +14,19 @@ function GameList() {
         }
     };
     const URL = `https://api-american-football.p.rapidapi.com/games?date=2023-10-08&league=1&season=${urlSeason}&timezone=America%2FNew_York`;
-    const [game, setGame] = useState(null);
+    const [game, setGame] = useState([]);
+    
 
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(URL, options);
+
             const result = await response.text();
             const gamesResult = JSON.parse(result)
             const games = gamesResult.response;
             setGame(games)
+
         
 }
 fetchData();
@@ -30,15 +34,27 @@ fetchData();
 
 console.log(game)
 return (
-    <ul>
+    <ul className="games-list">
         {game.map(gameObject => (
+            
           <li key={gameObject.game.id}>
-            {gameObject.game.stage}
-            <div>
-                {gameObject.teams.home.name}
+            
+            <div >
+            <img src={gameObject.teams.home.logo}/>
+            <p>{gameObject.teams.home.name}</p>
+                <span>{gameObject.scores.home.total}</span>
             </div>
             <div>
-                {gameObject.teams.away.name}
+                <p>{gameObject.game.stage}</p>
+                <p>{gameObject.game.date.time}</p>
+                <p>Timezone:{gameObject.game.date.timezone}</p>
+                <p>Status:{gameObject.game.status.long}</p>
+
+            </div>
+            <div >
+                <img src={gameObject.teams.away.logo}/>
+                <p>{gameObject.teams.away.name}</p>
+                <span>{gameObject.scores.away.total}</span>
             </div>
             </li>
         ))}
